293ce96f51917f2538c941d600d058573b7703f0
Yuki <arduino0420@gmail.com>
2026-08-14 11:11:40 +0900

    Fix task translations and user edit validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, on: :update
+  validates :password, length: { minimum: 6 }, allow_blank: true
 end
